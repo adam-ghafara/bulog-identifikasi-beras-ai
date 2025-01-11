a995ef8f545861d6666af4419005ac4a94e7fd1c
adam-ghafara <adam.ghafara.job@gmail.com>
2025-01-11 08:03:44 +0700

    Renew AI model and updating minor fix

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import tensorflow as tf
 from PIL import Image
 import numpy as np
 import io
+import requests
 
 app = Flask(__name__, template_folder='templates')
 
@@ -13,8 +14,19 @@ app = Flask(__name__, template_folder='templates')
 ####
 ########################
 
-## This variable is meant to be used to load the model from Google Drive, but it is not working in this case. Use this variable wisely to avoid unnecessary errors.
-# model_url='https://drive.google.com/file/d/1R44ysaM2ldbSXfWNecUdj2iN_GMThHFS/view?usp=sharing'
+# model_url='https://drive.google.com/uc?export=download&id=44ysaM2ldbSXfWNecUdj2iN_GMThHFS'
+# model_loader = requests.get(model_url, stream=True)
+# with open('./model/rice_model.h5', 'wb') as file:
+#     for chunk in response.iter_content(1024):
+#         if chunk:
+#             file.write(chunk)
+
+# Load The Model from the URL directory
+# model_from_url = tf.keras.models.load_model(model_loader)
+
+# step_1 = tf.keras.models.load_model('./model/rice_model.h5')
+# step_2 = tf.keras.models.load_model('./model/rice_model.h5')
+# step_3 = tf.keras.models.load_model('./model/rice_model.h5')
 
 model = tf.keras.models.load_model('./model/rice_model.h5')
 
@@ -69,6 +81,38 @@ def identify():
             model_accuracy = prediction[0][predicted_class] * 100
             return render_template('hasil-identifikasi.html', image=image_render, predicted_class=predicted_class, final_accuracy=model_accuracy)
     return render_template('identifikasi.html')
+
+# Identify with 2 steps (Still in Development)
+# @app.route('/identify', methods=['GET', 'POST'])
+# def identify():
+#     if request.method == 'POST':
+#         file = request.files['image']
+#         if file:
+#             img = Image.open(io.BytesIO(file.read()))
+#             # Preprocess the image
+#             preprocessed_image = image_loader(img, target=(256, 256))
+#             preprocessed_image = np.array(preprocessed_image, dtype='float32')
+#             # Predict the image (Step 1)
+#             prediction = step_1.predict(preprocessed_image)
+#             # Step 1 Class
+#             step_1_class = class_labels[np.argmax(prediction)]
+#             # Step 1 Accuracy
+#             step_1_accuracy = prediction[0][step_1_class] * 100
+#             # Predict the image (Step 2)
+#             prediction = step_2.predict(preprocessed_image)
+#             # Step 2 Class
+#             step_2_class = class_labels[np.argmax(prediction)]
+#             # Step 2 Accuracy
+#             step_2_accuracy = prediction[0][step_2_class] * 100
+#             # Process the image to be displayed
+#             img.seek(0)
+#             image_stream = io.BytesIO()
+#             img.save(image_stream, format='PNG')
+#             image_render = image_stream.getvalue()
+#             image_render = "data:image/png;base64," + base64.b64encode(image_render).decode('utf-8')
+#             # Display to HTML
+#             return render_template('hasil-identifikasi.html', image=image_render, step_1_prediction=step_1_class, step_1_accuracy=step_1_accuracy, step_2_prediction=step_2_class, step_2_accuracy=step_2_accuracy)
+#     return render_template('identifikasi.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
